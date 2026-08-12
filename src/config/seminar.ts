f4b0dc39d74ca6421b87d922bde81865c4c9a14f
unknown <cyasar@comu.edu.tr>
2026-08-12 19:44:57 +0300

    Consolidate all Colab URLs into a single notebook link

--- src/config/seminar.ts
+++ src/config/seminar.ts
@@ -1,6 +1,6 @@
 export const seminarConfig = {
-  colabGroverUrl: "https://colab.research.google.com/github/qiskit-community/qiskit-textbook/blob/main/content/ch-algorithms/grover.ipynb",
-  colabShorUrl: "https://colab.research.google.com/github/qiskit-community/qiskit-textbook/blob/main/content/ch-algorithms/shor.ipynb",
-  colabQaoaUrl: "https://colab.research.google.com/github/qiskit-community/qiskit-textbook/blob/main/content/ch-applications/qaoa.ipynb",
+  colabGroverUrl: "https://colab.research.google.com/drive/1QHDvCQCcGDUFZ9BIXvJdmDrgSJO1PZ09?usp=sharing",
+  colabShorUrl: "https://colab.research.google.com/drive/1QHDvCQCcGDUFZ9BIXvJdmDrgSJO1PZ09?usp=sharing",
+  colabQaoaUrl: "https://colab.research.google.com/drive/1QHDvCQCcGDUFZ9BIXvJdmDrgSJO1PZ09?usp=sharing",
   totalSlides: 35,
 };
